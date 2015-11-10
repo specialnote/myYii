00fb861cfa6bd93be22349e66d6c4f7aac502e03
specialnote-yang <2022281825@qq.com>
2015-11-10 16:43:37 +0800

    修改

--- frontend/controllers/ArticleController.php
+++ frontend/controllers/ArticleController.php
@@ -2,32 +2,48 @@
     namespace frontend\controllers;
 
     use common\models\Article;
+    use common\models\ArticleTag;
     use common\models\Category;
     use common\models\Tag;
     use yii\data\Pagination;
+    use yii\helpers\ArrayHelper;
+    use yii\helpers\Html;
     use yii\web\NotFoundHttpException;
     use Yii;
 
     class ArticleController extends BaseController{
         public $layout = 'mini';
 
+        /**
+         * 获取文章列表（可以按分类、标签获取文章列表）
+         * @return string
+         */
         public function actionIndex(){
-            $data = Article::find()->where(['status'=>Article::STATUS_DISPLAY])->orderBy(['publish_at'=>SORT_DESC]);
+            $category_id = Html::encode(Yii::$app->request->get('category_id'));
+            $tag_id = Html::encode(Yii::$app->request->get('tag_id'));
+            $data = Article::find()->where(['status'=>Article::STATUS_DISPLAY,'category_id'=>$category_id]);
+            if($tag_id){
+                $article_tags = ArticleTag::find()->where(['tag_id'=>$tag_id])->all();
+                $article_ids = ArrayHelper::getColumn($article_tags,'article_id');
+                $data = $data->andWhere(['in','id',$article_ids]);
+            }
             $pages = new Pagination(['totalCount'=>$data->count(),'pageSize'=>20]);
 
             //全部文章
-            $articles = $data->offset($pages->offset)->limit($pages->limit)->all();
+            $articles = $data->orderBy(['publish_at'=>SORT_DESC])->offset($pages->offset)->limit($pages->limit)->all();
             //推荐文章
             $recommendArticles = Article::getRecommendArticle();
             //分类
             $categories = Category::find()->where(['status'=>Category::STATUS_DISPLAY])->all();
-            //标签
+            //热门标签
             $tags = Tag::find()->orderBy(['article_count'=>SORT_DESC,'id'=>SORT_DESC])->limit(10)->all();
             return $this->render('index',[
                'articles'=>$articles,
                 'recommendArticles'=>$recommendArticles,
                 'categories'=>$categories,
-                'tags'=>$tags
+                'tags'=>$tags,
+                'category_id'=>$category_id,
+                'tag_id'=>$tag_id,
             ]);
         }
 
--- frontend/views/article/index.php
+++ frontend/views/article/index.php
@@ -5,13 +5,13 @@
 ?>
 <div class="row">
     <ul class="category-menu">
-        <li>
+        <li class="<?= (empty($category_id))?'menu_selected':'' ?>">
             <a href="<?= Url::to(['/article/index']) ?>">全部</a>
         </li>
         <?php foreach($categories as $category): ?>
             <?php if($category->name): ?>
-                <li>
-                    <a href="<?= Url::to(['/article/index','category'=>$category->id]) ?>">
+                <li class="<?= ($category_id==$category->id)?'menu_selected':'' ?>">
+                    <a href="<?= Url::to(['/article/index','category_id'=>$category->id]) ?>">
                         <?= $category->name ?>
                     </a>
                 </li>
@@ -68,6 +68,22 @@
                                 <span><?= $k+1 ?></span>
                                 <a target="_blank" href="<?= Url::to(['/article/detail','id'=>$article->id]) ?>"><?= $article->title ?> </a>
                             </div>
+                        </li>
+                    <?php endforeach;?>
+                </ul>
+            </div>
+        </div>
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h2 class="panel-title">热门标签</h2>
+            </div>
+            <div class="panel-body">
+                <ul class="media-lis article-tag">
+                    <?php foreach($tags as $tag): ?>
+                        <li class="<?= ($tag_id == $tag->id)?'tag_selected':'' ?>">
+                            <a href="<?= Url::to(['/article/index','tag_id'=>$tag->id]) ?>">
+                                <?= $tag->id.$tag->name.'<font style="color:#555">('.$tag->article_count.')</font>' ?>
+                            </a>
                         </li>
                     <?php endforeach;?>
                 </ul>
