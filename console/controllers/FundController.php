e6467087b6bec8c781277a11b17d834f6fb49800
specialnote-yang <2022281825@qq.com>
2016-01-07 14:04:12 +0800

    获取爱基金上个交易日基金数据

--- console/controllers/FundController.php
+++ console/controllers/FundController.php
@@ -6,20 +6,24 @@ use common\models\FundData;
 use common\models\FundLog;
 use common\models\Gather;
 use yii\console\Controller;
+use yii\helpers\ArrayHelper;
 use yii\web\NotFoundHttpException;
 use Goutte\Client;
 
 class FundController extends Controller{
     private $url = [
-        //'http://fund.ijijin.cn/data/Net/info/zqx_F009_desc_0_0_1_9999_0_0_0_jsonp_g.html',
-        'http://fund.ijijin.cn/data/Net/info/hhx_F009_desc_0_0_1_9999_0_0_0_jsonp_g.html',
-        //'http://fund.ijijin.cn/data/Net/info/gpx_F009_desc_0_0_1_9999_0_0_0_jsonp_g.html',
+        'zqx'=>'http://fund.ijijin.cn/data/Net/info/zqx_F009_desc_0_0_1_9999_0_0_0_jsonp_g.html',//债券型
+        'hhx'=>'http://fund.ijijin.cn/data/Net/info/hhx_F009_desc_0_0_1_9999_0_0_0_jsonp_g.html',//混合
+        'gpx'=>'http://fund.ijijin.cn/data/Net/info/gpx_F009_desc_0_0_1_9999_0_0_0_jsonp_g.html',//股票型
+        'zsx'=>'http://fund.ijijin.cn/data/Net/info/zsx_F009_desc_0_0_1_9999_0_0_0_jsonp_g.html',//指数型
+        'bbx'=>'http://fund.ijijin.cn/data/Net/info/bbx_F009_desc_0_0_1_9999_0_0_0_jsonp_g.html',//保本型
     ];
 
-    //采集基金基本详情
+    //采集爱基金上个交易日数据
     public function actionData(){
+        @set_time_limit(0);
         $client = new Client();
-        foreach($this->url as $u){
+        foreach($this->url as $type=>$u){
             $crawler = $client->request('GET', $u);
             $result = $crawler->text();
 
@@ -27,9 +31,24 @@ class FundController extends Controller{
             $result = trim($result,'(');
             $result = trim($result,')');
             $data_area = json_decode($result,true);
+            $k = 0;
             foreach($data_area['data']['data'] as $key=>$v) {
-               $this->insertData($v);
+                if($k>5)break;
+                $this->insertData($v,$type);
+                $k ++;
             }
+            $this->saveSort($type);
+        }
+
+    }
+
+    public function saveSort($type){
+        $rate_data = ArrayHelper::getColumn(FundData::find()->where(['fund_type'=>$type])->select('rate')->distinct()->orderBy(['rate'=>SORT_DESC])->asArray()->all(),'rate');
+        $data = FundData::find()->where(['fund_type'=>$type])->all();
+        foreach($data as $k=>$v){
+            $sort = array_search($v->rate,$rate_data);
+            $v->day_sort = $sort+1;
+            $v->save(false);
         }
     }
 
@@ -41,9 +60,10 @@ class FundController extends Controller{
     /**
      * 采集基金详情
      * @param $fundData
+     * @param $type
      * @return bool
      */
-    private function insertData($fundData){
+    private function insertData($fundData,$type){
         $iopv = $fundData['net'];
         $accnav = $fundData['totalnet'];
         $growth = $fundData['ranges'];
@@ -51,31 +71,45 @@ class FundController extends Controller{
         $num = $fundData['code'];
         $date = $fundData['SYENDDATE'];
         if($num && $date && $iopv && $accnav && $growth && $rate){
-            if(self::isFund($num)){
-               try{
-                   $fundData = new FundData();
-                   $fundData->date = $date;
-                   $fundData->fund_num = $num;
-                   $fundData->iopv = $iopv;
-                   $fundData->accnav = $accnav;
-                   $fundData->growth = $growth;
-                   $fundData->rate = $rate;
-                  $res =  $fundData->save();
-
-                   if($res){
-                       //基金详情采集日志
-                       FundLog::insertFundLog($num,$date,FundLog::ITEM_FUND_DATA);
-                       return true;
-                   }else{
-                       return false;
-                   }
-               }catch(\Exception $e){
-                   echo $e->getMessage().PHP_EOL;
+            $month = date('n',strtotime($date));
+            if($month<=3){
+                $quarter = 1;
+            }elseif($month<=6){
+                $quarter = 2;
+            }elseif($month<=9){
+                $quarter = 3;
+            }elseif($month<=12){
+                $quarter = 4;
+            }else{
+                $quarter = 0;
+            }
+           try{
+               $fundData = new FundData([
+                   'date'=>$date,
+                   'week'=>date('W',strtotime($date)),
+                   'month'=>$month,
+                   'quarter'=>$quarter,
+                   'year'=>date('Y',strtotime($date)),
+                   'fund_num'=>$num,
+                   'fund_type'=>$type,
+                   'iopv'=>$iopv,
+                   'accnav'=>$accnav,
+                   'growth'=>$growth,
+                   'rate'=>$rate
+               ]);
+               $res =  $fundData->save();
+               if($res){
+                   //基金详情采集日志
+                   FundLog::insertFundLog($num,$date,FundLog::ITEM_FUND_DATA);
+                   return true;
+               }else{
                    return false;
                }
-            }else{
-                return false;
-            }
+           }catch(\Exception $e){
+               echo $e->getMessage().PHP_EOL;
+               return false;
+           }
+
         }else{
             return false;
         }
--- console/migrations/m151117_111650_fund_data.php
+++ console/migrations/m151117_111650_fund_data.php
@@ -9,14 +9,18 @@ class m151117_111650_fund_data extends Migration
     {
         $tableOptions = null;
         if ($this->db->driverName === 'mysql') {
-            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB COMMENT "基金净值表"';
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB COMMENT "基金数据表"';
         }
+        $this->dropTable('{{%fund_data}}');
         $this->createTable('{{%fund_data}}', [
             'id' => $this->primaryKey(),
             'fund_num'=>$this->string(50),
+            'fund_type'=>$this->string(50),//类型
             'date' => $this->string(10),
+            'day_sort' => $this->string(10),//当天排名
             'week' => $this->string(10),//一年第几周
             'month' => $this->string(10),//一年第几月
+            'quarter' => $this->string(10),//一年第几季度
             'year' => $this->string(10),//第几年
             'iopv' => $this->string(10),//基金单位净值
             'accnav' => $this->string(10),//基金累计净值
@@ -25,7 +29,6 @@ class m151117_111650_fund_data extends Migration
             'created_at' => $this->integer(10)->notNull(),
             'updated_at' => $this->integer(10)->notNull(),
         ], $tableOptions);
-
     }
 
     public function down()
